6e8c36febd727bd2bed573b16490ab2315089268
Aleesha Naxakis <aleesha-@hotmail.com>
2024-01-15 22:00:20 +1100

    improved file structure and added User.js model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const Bcrypt = require('bcrypt');
+
+const challengeSchema = require('./Challenge');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/,'Must use a valid e-mail address.']
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    challenges: [challengeSchema], 
+}
+{
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return Bcrypt.compare(password,this.password);
+}
+    userSchema.virtual("ChallengeCount").get(function() {
+        return this.challenges.length
+    })
+    const User = model("User", userSchema);
+    
+    module.exports = User;
