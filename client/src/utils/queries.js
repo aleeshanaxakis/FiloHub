0841e526bb4df49f15c877736f200075cd242ec8
Aleesha Naxakis <aleesha-@hotmail.com>
2024-01-25 21:22:33 +1100

    added queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,30 @@
-// Set this up useMutation 18 for Week 21
+import { gql } from '@apollo/client';
+
+export const QUERY_CHALLENGES = gql`
+  query getChallenges {
+    challenges {
+      _id
+      creator
+      createdAt
+      title
+      challengeBody
+    }
+  }
+`;
+
+export const QUERY_SINGLE_CHALLENGE = gql`
+  query getSingleChallenge($challengeId: ID!) {
+    challenge(challengeId: $challengeId) {
+      _id
+      creator
+      createdAt
+      title
+      challengeBody
+      participants {
+        _id
+        name
+        createdAt
+      }
+    }
+  }
+`;
