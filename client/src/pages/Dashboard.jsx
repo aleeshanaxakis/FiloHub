fe8a4719d9aaaf48c4ee68107fca0ded0b11f4ea
Aleesha Naxakis <aleesha-@hotmail.com>
2024-02-06 18:04:32 +1100

    updated Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
+import React from 'react';
 import { useQuery } from '@apollo/client';
 import { Container, Header, Loader } from 'semantic-ui-react';
-
 import { GET_CHALLENGES } from '../utils/queries';
 
 const Dashboard = () => {
-    const { loading, data } from - useQuery(GET_ME)
+    const { loading, data } = useQuery(GET_CHALLENGES);
+
+    return (
+        <Container>
+      <Header as="h2">My Challenges</Header>
+      {loading ? (
+        <Loader active>Loading...</Loader>
+      ) : (
+        <div>
+          {/* Display list of challenges */}
+          {data && data.challenges.map(challenge => (
+            <div key={challenge._id}>
+              <h3>{challenge.title}</h3>
+              <p>{challenge.challengeBody}</p>
+              <p>Creator: {challenge.creator}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </Container>
+  );
 }
+
+export default Dashboard;
