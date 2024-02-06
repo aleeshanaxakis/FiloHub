13a1862461f98dabd7ee2c3f1e79712e95221d28
Aleesha Naxakis <aleesha-@hotmail.com>
2024-02-06 22:21:15 +1100

    fixed an error in server.js

--- server/server.js
+++ server/server.js
@@ -38,10 +38,10 @@ async function startServer() {
 
 // Serve static files from the React app in production
   if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, '../client/build')));
+      app.use(express.static(path.join(__dirname, '../client/dist')));
 
       app.get('*', (req, res) => {
-          res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+          res.sendFile(path.join(__dirname, '../client/dist/index.html'));
       });
   }
  
