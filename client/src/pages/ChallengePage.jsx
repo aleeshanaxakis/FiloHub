b926ada16f42e7142e70465cab89d2c4de3fb663
Aleesha Naxakis <aleesha-@hotmail.com>
2024-02-06 18:06:36 +1100

    modified ChallengePage.jsx

--- client/src/pages/ChallengePage.jsx
+++ client/src/pages/ChallengePage.jsx
@@ -1,7 +1,28 @@
-// import { useQuery } from '@apollo/client';
-// import { useMutation } from '@apollo/client';
-// import { GET_CHALLENGES } from '../utils/queries';
-// import { GET_CHALLENGE } from '../utils/queries';
-// import { DELETE_CHALLENGE } from '../utils/mutations';
-// import Auth from '../utils/auth';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { Container, Header, Loader } from 'semantic-ui-react';
+import { GET_CHALLENGE } from '../utils/queries';
 
+const ChallengePage = () => {
+    const { challengeId } = useParams();
+    const { loading, data } = useQuery(GET_CHALLENGE, {
+      variables: { challengeId }
+    });
+  
+    return (
+      <Container>
+        {loading ? (
+          <Loader active>Loading...</Loader>
+        ) : (
+          <div>
+            <Header as="h2">{data.challenge.title}</Header>
+            <p>{data.challenge.challengeBody}</p>
+            <p>Creator: {data.challenge.creator}</p>
+          </div>
+        )}
+      </Container>
+    );
+  }
+  
+  export default ChallengePage;
